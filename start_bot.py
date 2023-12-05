d681f7d38a321eef62d1652090f2dab89150676c
MrJayrus <jayrofidencio@gmail.com>
2023-12-05 22:32:02 +0100

    Manejar excepciones y codigo corregido

--- start_bot.py
+++ start_bot.py
@@ -6,31 +6,35 @@ import time
 #Instanciar el Bot
 bot = telebot.TeleBot(telegram_token)
 
-#Responde a las que no son comandos
-@bot.message_handler(content_types="text")  
-def bot_messages_texto(message):
-     #Para Gestionar los Mensajes de textos recibidos
-    if bot_messages_texto.text.startswith("/"):      
-        bot.send_message(bot_messages_texto.chat.id, "Comando no Disponible")
-    else:    
-        x = bot.send_message(bot_messages_texto.chat.id, "</b><HOLA></b>", parse_mode="html",disable_web_page_preview=True )
-    time.sleep(3)
-    bot.edit_message_text("<u><HOLA></u>", bot_messages_texto.chat.id, x.message_id, parse_mode="html" )
+#Responde a los mensajes de texto
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    try:# Logica a ejecutar
+        help_command(bot, message)
+        user_id = message.from_user.id
+        pass
+    except Exception as e:# En caso de error
+        print(f"Error al manejar el mensaje: {e}")
 
 # Manejar mensajes /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    start_command(bot, message)
-    user_id = message.from_user.id
+    try:
+        start_command(bot, message)
+        user_id = message.from_user.id
+    except Exception as e:
+        print(f"Error al manejar el comando /start: {e}")
 
 # Manejar mensajes /help
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-    help_command(bot, message)
-    user_id = message.from_user.id
+    try:
+        help_command(bot, message)
+        user_id = message.from_user.id
+    except Exception as e:
+        print(f"Error al manejar el comando /help: {e}")
 
 #Escribir el Programa Principal.
-    if __name__ == '__main__':
-        print('Iniciando el Bot')
-        bot.infinity_polling()
-        print('Fin')
+print('Iniciando el Bot')
+bot.infinity_polling()
+print('Fin')
