a90363927b5addbf1c8162f2dcacc8f96bf06ea2
MrJayrus <jayrofidencio@gmail.com>
2023-12-05 22:29:22 +0100

    correccion del codigo

--- basics.py
+++ basics.py
@@ -1,5 +1,5 @@
 #Responder a /start
-def help_command(bot, message):
+def start_command(bot, message):
     bot.reply_to(message, "Hola, Bienvenidos a mi Bot",parse_mode='Markdown')
 
 #Responder a /help
