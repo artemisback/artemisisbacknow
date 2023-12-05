04929ba65fbb5c0e50d5e3914b46e04b74201023
MrJayrus <jayrofidencio@gmail.com>
2023-12-05 21:31:38 +0100

    Comandos /start y help ahora cargan en un modulo separado

--- start_bot.py
+++ start_bot.py
@@ -1,15 +1,11 @@
+from basics import * #Importar modulo basics
 from config import * #Importar el Token
 import telebot #Para la Api de Telegram
 import time
 
 #Instanciar el Bot
 bot = telebot.TeleBot(telegram_token)
-#Responder al Comando start
-@bot.message_handler(commands=["start","ayuda","help"])
-#Responde al Comando /start
-def cmd_start(message):
-    #"Da la Bienvenida al usuario del Bot"
-    bot.reply_to(message, "Hola, Bienvenidos a mi Bot")
+
 #Responde a las que no son comandos
 @bot.message_handler(content_types="text")  
 def bot_messages_texto(message):
@@ -20,7 +16,19 @@ def bot_messages_texto(message):
         x = bot.send_message(bot_messages_texto.chat.id, "</b><HOLA></b>", parse_mode="html",disable_web_page_preview=True )
     time.sleep(3)
     bot.edit_message_text("<u><HOLA></u>", bot_messages_texto.chat.id, x.message_id, parse_mode="html" )
-    
+
+# Manejar mensajes /start
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    start_command(bot, message)
+    user_id = message.from_user.id
+
+# Manejar mensajes /help
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    help_command(bot, message)
+    user_id = message.from_user.id
+
 #Escribir el Programa Principal.
     if __name__ == '__main__':
         print('Iniciando el Bot')
