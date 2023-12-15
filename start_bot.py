04b4f570425512f24e79fe656726a5da81519b7a
MrJayrus <jayrofidencio@gmail.com>
2023-12-15 16:58:15 -0500

    Actualizacion

--- start_bot.py
+++ start_bot.py
@@ -1,7 +1,7 @@
-from basics import * #Importar modulo para manejar algunas respuestas
-from config import * #Importar el Token
-from visuales import new_visual #Importar modulo para trabajar los visuales
-import telebot #Para la Api de Telegram
+from basics import * # Importar modulo para manejar algunas respuestas
+from config import * # Importar el Token
+from visuales import new_visual # Importar modulo para trabajar los visuales
+import telebot # Para la Api de Telegram
 
 #Instanciar el Bot
 bot = telebot.TeleBot(telegram_token)
@@ -30,7 +30,7 @@ def handle_newvisual(message):
     except Exception as e:# En caso de error
         print(f"Error al manejar el comando /newvisual: {e}")
 
-@bot.message_handler(func=lambda message: True)#Responde a los mensajes de texto y comandos desconocidos
+@bot.message_handler(func=lambda message: True)# Responde a los mensajes de texto y comandos desconocidos
 def handle_message(message):
     try:# Logica a ejecutar
         unknw_command(bot, message)
@@ -39,7 +39,7 @@ def handle_message(message):
     except Exception as e:# En caso de error
         print(f"Error al manejar el mensaje desconocido: {e}")
 
-#Iniciar bot y mostrar estado en consola
+# Iniciar bot y mostrar estado en consola
 print('Bot a la escucha...')
 bot.infinity_polling()
 print('Bot apagado...')
