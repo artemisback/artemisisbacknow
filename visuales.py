e8b7bd382c67972bf746567381d8000e8d98a1ae
MrJayrus <jayrofidencio@gmail.com>
2023-12-15 17:05:53 -0500

    Ahora se salvan los datos en la base de datos

--- visuales.py
+++ visuales.py
@@ -1,11 +1,11 @@
 from db_works import load_visuales_db, save_visuales_db # IMPORTAMOS LAS FUNCIONES NECESARIAS
 
-def new_visual(bot, message):#Preguntar por el nombre
+def new_visual(bot, message):# Preguntar por el nombre
     user_id = message.from_user.id
     bot.send_message(user_id, "💠 Para establecer un nuevo visual necesito varios datos, empecemos por el nombre, envíamelo a continuacion:\n\nRecuerda que puede cancelar la operación en cualquier momento enviando la cadena: (cancelar)", parse_mode='Markdown')
     bot.register_next_step_handler(message, obtener_nombre_del_visual, bot)
 
-def obtener_nombre_del_visual(message, bot):#Verificar el nombre y pedir tipo
+def obtener_nombre_del_visual(message, bot):# Verificar el nombre y pedir tipo
     user_id = message.from_user.id
     longitud_del_nombre = len(message.text)
     if message.text == "cancelar":
@@ -18,7 +18,7 @@ def obtener_nombre_del_visual(message, bot):#Verificar el nombre y pedir tipo
         bot.send_message(user_id, "❌ Enviaste un nombre muy largo!\n\nEl límite de caracteres para el nombre es de 60.\n\nVuelve a intentarlo.", parse_mode='Markdown')
         bot.register_next_step_handler(message, obtener_nombre_del_visual, bot)
 
-def obtener_tipo_del_visual(message, nombre_del_visual, bot):#Verificar tipo y pedir los detalles
+def obtener_tipo_del_visual(message, nombre_del_visual, bot):# Verificar tipo y pedir los detalles
     user_id = message.from_user.id
     longitud_del_tipo = len(message.text)
     if message.text == 'cancelar':
@@ -31,7 +31,7 @@ def obtener_tipo_del_visual(message, nombre_del_visual, bot):#Verificar tipo y p
         bot.send_message(user_id, "❌ Enviaste un mensaje muy largo o incorrecto!\n\nEl límite de caracteres para el nombre es de 20.\n\nVuelve a intentarlo.", parse_mode='Markdown')
         bot.register_next_step_handler(message, obtener_tipo_del_visual, nombre_del_visual, bot)
 
-def obtener_detalles_del_visual(message, tipo_del_visual, nombre_del_visual, bot):#Verifiicar los detalles y pedir la fecha de estreno
+def obtener_detalles_del_visual(message, tipo_del_visual, nombre_del_visual, bot):# Verifiicar los detalles y pedir la fecha de estreno
     user_id = message.from_user.id
     longitud_del_detalle = len(message.text)
     if message.text == 'cancelar':
@@ -44,7 +44,7 @@ def obtener_detalles_del_visual(message, tipo_del_visual, nombre_del_visual, bot
         bot.send_message(user_id, "❌ Enviaste un mensaje muy largo!\n\nEl límite de caracteres para el nombre es de 400.\n\nVuelve a intentarlo.", parse_mode='Markdown')
         bot.register_next_step_handler(message, obtener_detalles_del_visual, tipo_del_visual, nombre_del_visual, bot)
 
-def obtener_estreno_del_visual(message, detalles_del_visual, tipo_del_visual, nombre_del_visual, bot):#Verifiicar estreno y pedir genero
+def obtener_estreno_del_visual(message, detalles_del_visual, tipo_del_visual, nombre_del_visual, bot):# Verifiicar estreno y pedir genero
     user_id = message.from_user.id
     longitud_del_estreno = len(message.text)
     if message.text == 'cancelar':
@@ -57,7 +57,7 @@ def obtener_estreno_del_visual(message, detalles_del_visual, tipo_del_visual, no
         bot.send_message(user_id, "❌ Enviaste un mensaje muy largo!\n\nEl límite de caracteres para el nombre es de 30.\n\nVuelve a intentarlo.", parse_mode='Markdown')
         bot.register_next_step_handler(message, obtener_estreno_del_visual, detalles_del_visual, tipo_del_visual, nombre_del_visual, bot)
 
-def obtener_genero_del_visual(message, estreno_del_visual, detalles_del_visual, tipo_del_visual, nombre_del_visual, bot):#Verifiicar el genero y guardar datos
+def obtener_genero_del_visual(message, estreno_del_visual, detalles_del_visual, tipo_del_visual, nombre_del_visual, bot):# Verifiicar el genero y guardar datos
     user_id = message.from_user.id
     longitud_del_genero = len(message.text)
     if message.text == 'cancelar':
@@ -66,8 +66,20 @@ def obtener_genero_del_visual(message, estreno_del_visual, detalles_del_visual, 
         genero_del_visual = message.text
         bot.send_message(user_id, "💠 Estoy guardando los datos espera un momento...", parse_mode='Markdown')
         # AQUI VA LA MECANICA PARA GUARDAR LOS DATOS NUEVOS EN LA DB (YO ME ENCARGO DE ESTO)
-        bot.send_message(user_id, f"💠 LISTO:\n\nLos datos son:\n{nombre_del_visual}\n{tipo_del_visual}\n{detalles_del_visual}\n{estreno_del_visual}\n{genero_del_visual}", parse_mode='Markdown')
-        print("SALVADO CON EXITO")
+        
+        nuevo_visual = {
+        "Nombre": nombre_del_visual,
+        "Tipo": tipo_del_visual,
+        "Detalles": detalles_del_visual,
+        "Estreno": estreno_del_visual,
+        "Genero": genero_del_visual
+        }
+        try:
+            save_visuales_db(nuevo_visual)
+            bot.send_message(user_id, f"💠 LISTO\n\nLos datos del nuevo visual se guardaron con exito en la base de datos.", parse_mode='Markdown')
+            print("SALVADO CON EXITO")
+        except:
+            bot.send_message(user_id, f"❌ ERROR\n\nLos datos del nuevo visual no se guardaron.", parse_mode='Markdown')
     else:
         bot.send_message(user_id, "❌ Enviaste un mensaje muy largo o incorrecto!\n\nEl límite de caracteres para el nombre es de 50.\n\nVuelve a intentarlo.", parse_mode='Markdown')
         bot.register_next_step_handler(message, obtener_genero_del_visual, estreno_del_visual, detalles_del_visual, tipo_del_visual, nombre_del_visual, bot)
